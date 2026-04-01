704a1ec5c62a44707523b0dfd2dba52adfca0095
syedibrahim0325-cloud <syedibrahim0325@gmail.com>
2026-04-01 23:25:47 +0530

    HelloApp files uc Uploaded

---
+++ HelloApp2/HelloApp5.java
@@ -0,0 +1,22 @@
+public class HelloApp5 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+
+       
+        System.out.println("Hello, " + nameBuilder.toString() + "!");
+    }
+}
